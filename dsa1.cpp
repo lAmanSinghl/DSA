481627b81e43bcdef43198a641b15a83757b5b16
lAmanSinghl <120amansingh@gmail.com>
2026-08-06 13:13:49 +0530

    Day-1

--- dsa1.cpp
+++ dsa1.cpp
@@ -531,21 +531,21 @@ void patter21(int n)
 void patter22(int n)
 {
     // pattern->
-    //  *  *  *  *  *
-    //  *           *
-    //  *           *
-    //  *           *
-    //  *  *  *  *  *
+    // 33333
+    // 32223
+    // 32123
+    // 32223
+    // 33333
 
-    for (int i = 1; i <=2*n-1; i++)
+    for (int i = 1; i <= 2 * n - 1; i++)
     {
-        for (int j = 1; j <= 2*n-1; j++)
+        for (int j = 1; j <= 2 * n - 1; j++)
         {
-           int top=i-1;
-           int left=j-1;
-           int right=2*n-i-1;
-           int bottom=2*n-j-1;
-           cout<<n-min(min(top,bottom),min(left,right));
+            int top = i - 1;
+            int left = j - 1;
+            int right = 2 * n - i - 1;
+            int bottom = 2 * n - j - 1;
+            cout << n - min(min(top, bottom), min(left, right));
         }
         cout << endl;
     }
